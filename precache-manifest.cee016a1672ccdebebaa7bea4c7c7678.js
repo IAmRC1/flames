280a979d010c80b46cf3a19db1d9a0edc4874a97
Rishabh Singh <rishabhcena1@gmail.com>
2020-04-27 00:32:10 +0530

    Updates

---
+++ precache-manifest.cee016a1672ccdebebaa7bea4c7c7678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3364101b1d4bca4b85cfb98525686e88",
+    "url": "/flames/index.html"
+  },
+  {
+    "revision": "43814407ad4fd6d011a9",
+    "url": "/flames/static/css/main.fa02e475.chunk.css"
+  },
+  {
+    "revision": "1880beb14bef9fc9bdc4",
+    "url": "/flames/static/js/2.0927ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flames/static/js/2.0927ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43814407ad4fd6d011a9",
+    "url": "/flames/static/js/main.f31732a6.chunk.js"
+  },
+  {
+    "revision": "d174b8827aa43e1b8581",
+    "url": "/flames/static/js/runtime-main.4ab32478.js"
+  },
+  {
+    "revision": "a44d48904e8774507aae4d18bd147f1d",
+    "url": "/flames/static/media/uu.a44d4890.png"
+  }
+]);
